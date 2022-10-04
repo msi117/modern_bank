446260c4d1fd9775a041c7aca4f1acbaf0b93434
Mir Sameer <mir.sameer@example.com>
2022-10-04 19:13:57 +0530

    Stage 1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h1 className="text-4xl font-bold underline text-red-200">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
